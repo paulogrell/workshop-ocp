adffbb314df8e450ee6a7212a6bae722dd2b7e65
Paulo Grell <paulogrell@gmail.com>
2018-06-13 17:18:13 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,14 +1,14 @@
 node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'grell-node', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'workshop-ocp', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
     }
     stage('deploy') {
         input 'Manual Approval'
-        openshiftDeploy(deploymentConfig: 'grell-node')
+        openshiftDeploy(deploymentConfig: 'workshop-ocp')
     }
     stage('promoting to QA') {
        echo 'fake stage...'
